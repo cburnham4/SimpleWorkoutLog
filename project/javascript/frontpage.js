7267d012d0def68cb406f6916bb2eab6bf3aee29
Kendrick Hales <kjhales@live.unc.edu>
2016-12-05 18:10:59 -0500

    Error 400 update

--- project/javascript/frontpage.js
+++ project/javascript/frontpage.js
@@ -1,4 +1,4 @@
-var url_base = "http://wwwp.cs.unc.edu/Courses/comp426-f16/users/cvburha/project";
+var url_base = "http://wwwp.cs.unc.edu/Courses/comp426-f16/users/cvburnha/project";
 
 $(document).ready(function () {
 

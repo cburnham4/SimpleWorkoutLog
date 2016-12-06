bfe59962d046f3ef5bc05ca483e644bddade28ef
campoli <campoli@live.unc.edu>
2016-12-06 11:09:30 -0500

    routines

---
+++ project/backend/orm/Routine.php
@@ -0,0 +1,31 @@
+<?php
+date_default_timezone_set('America/New_York');
+class Routine
+{
+  public static function connect() {
+    return new mysqli("classroom.cs.unc.edu", 
+          "cvburnha", 
+          "tiger", 
+          "cvburnhadb");
+  }
+
+  public static function getRoutines($uid){
+    $mysqli = Muscle::connect();
+
+    $result = $mysqli->query("SELECT RID, RoutineName FROM Routines WHERE UserID = " . $uid);
+
+    $routines = array();
+
+    if($result){
+      while($next_row = $result->fetch_array()){
+        $json_obj = array('rid' => $next_row['RID'],
+                  'name' => $next_row['RoutineName']
+                  );
+        $routines[] = $json_obj;  
+      }
+    }
+
+    return $routines;
+  }
+
+}
---
+++ project/backend/routine-api.php
@@ -0,0 +1,22 @@
+<?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+
+    require_once("orm/Routine.php");
+
+
+  if ($_SERVER['REQUEST_METHOD'] == "GET") {
+
+    if (isset($_REQUEST['userId'])) {
+      $userId = trim($_REQUEST['userId']);
+
+      /* If only userid specified then get all exercises based on user */
+      header("Content-type: application/json");
+      print(json_encode(Routine::getRouines($userId)));
+      exit();
+    } 
+
+
+  } 
+?>

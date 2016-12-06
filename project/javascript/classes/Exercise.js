fff010404186a6a2adeb02433cca363b1fd7292d
campoli <campoli@live.unc.edu>
2016-12-06 17:44:19 -0500

    update

--- project/javascript/classes/Exercise.js
+++ project/javascript/classes/Exercise.js
@@ -2,4 +2,26 @@ var Exercise  = function(exercise_json){
 	this.eid = exercise_json.eid;
 	this.name = exercise_json.name;
 	this.mid = exercise_json.mid;
-}
+};
+
+Exercise.prototype.makeDiv = function() {
+    var cdiv = $("<div></div>");
+
+    $('#exercisediv').append('<tr><th>Number</th><th>Exercise</th></tr>');
+
+    cdiv.data('exercise', this);
+
+    return cdiv;
+};
+
+				// <div>
+				// 	<table class="table table-hover">
+				// 	    <tr>
+				// 	    	<th>Number</th>
+				// 		    <th>Exercise</th>
+				// 		</tr>
+
+
+				// 	</table>	
+
+				// </div>

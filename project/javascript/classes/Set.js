74f36a860ab6cd45eb93078268ac3d60a56335ce
campoli <campoli@live.unc.edu>
2016-12-07 17:55:34 -0500

    new forms

--- project/javascript/classes/Set.js
+++ project/javascript/classes/Set.js
@@ -4,3 +4,19 @@ var Set = function(set_json){
 	this.weight = set_json.weight;
 	this.did = set_json.did;
 };
+
+Set.prototype.makeDiv = function() {
+    var cdiv = $("<tr></tr>");
+
+    var tddiv = $("<td>reps: "+this.reps+" | weight: " + this.weight"</td>");
+
+    var tddiv2 = $('<td><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></td>');
+    tddiv2.addClass('deleteSet');
+
+    cdiv.append(tddiv);
+    cdiv.append(tddiv2);
+    
+    cdiv.data('set', this);
+
+    return cdiv;
+};

80e05ff643e16fb0d90f7316bbd214c9f029b17e
campoli <campoli@live.unc.edu>
2016-12-07 19:11:22 -0500

    fixed

--- project/javascript/classes/PastWorkout.js
+++ project/javascript/classes/PastWorkout.js
@@ -14,7 +14,7 @@ var PastWorkout = function(set_json){
     var fullDate = (monthNames[monthIndex] + ' ' + day + ' ' + year);
 
     var sets = [];
-    for(var i = 0; set_json.past_sets.length; i++){
+    for(var i = 0; i < set_json.past_sets.length; i++){
         sets[i] = new Set(set_json.past_sets[i]);
     }
 
@@ -22,10 +22,16 @@ var PastWorkout = function(set_json){
 
 PastWorkout.prototype.makeDiv = function() {
     var cdiv = $("<tr></tr>");
+    var td = $("<td></td>");
 
-    var tddiv = $('<td>reps: ' + this.reps + ' | weight: ' + this.weight +'</td>');
+    for (var i = 0; i < sets.length; i++) {
+        //var tddiv = $('<td>reps: ' + sets.reps + ' | weight: ' + this.weight +'</td>');
+        var tddiv = $('reps: ' + sets[i].reps + '| weight: ' + sets[i].weight + '<br>');
 
-    cdiv.append(tddiv);
+        td.append(tddiv);
+    }
+
+    cdiv.append(td);
     
     cdiv.data('pastworkout', this);
 

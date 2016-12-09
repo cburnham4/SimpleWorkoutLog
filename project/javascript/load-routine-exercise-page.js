1677d20d18400765a99c771d633b68a05e9eea68
Kendrick Hales <kjhales@live.unc.edu>
2016-12-08 20:16:31 -0500

    routine drop down

--- project/javascript/load-routine-exercise-page.js
+++ project/javascript/load-routine-exercise-page.js
@@ -4,19 +4,23 @@
 	var load_routine_exercise_page = function (routine) {
 
 		/* Get routine exercises into the routine exercise dropdown */
+		console.log("ran");
 		load_exercises_initial();
 
 
 	}
 
 	var load_exercises_initial = function(){
-		
-		var url_get = url_base + exercise_api;
+
+		var params = "?userId=" + userId;
+
+		var url_get = url_base + exercise_api + params;
 
 		$.ajax({
 	        url: url_get,
 	        type: 'GET',
 	        success: function(res) {
+	        	console.log(res);
 	        	for (var i=0; i<res.length; i++) {
 	   				var ex = new Exercise(res[i]);
 	   				$("#exercise-select").append(ex.makeOption());
